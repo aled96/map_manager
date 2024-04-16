43e7bb7cb2d305bb2c52094b0e8122de9782d727
Alessio De Luca <aled96@hotmail.it>
2024-04-16 11:25:07 +0200

    Expose service to change values of global maps

--- src/map_manager_server.cpp
+++ src/map_manager_server.cpp
@@ -11,6 +11,8 @@
 #include "std_msgs/msg/string.hpp"
 
 #include "map_manager/map_manager.hpp"
+#include "map_manager_srvs/srv/terrain_slope_update.hpp"
+#include "map_manager_srvs/srv/terrain_type_update.hpp"
 
 using namespace std::chrono_literals;
 using namespace std;
@@ -23,7 +25,9 @@ class MapManagerServer : public rclcpp::Node
     MapManagerServer()
     : Node("map_manager_server")
     {    
-        //Load static Image -- Terrain Type
+
+        //TO ACQUIRE
+        //Load Global Intial Static Image -- Terrain Type
         geometry_msgs::msg::Pose origin;
         origin.position.x = -0.495556;
         origin.position.y = -0.495556;
@@ -32,44 +36,125 @@ class MapManagerServer : public rclcpp::Node
 
         std::string filename = "/home/user/ros/marta_ws/src/map_manager/maps/mars_yard_traversability_areas_borders.pgm";
         std::string frame_id = "map";
-        std::string target_topic = "/map_terrain_type";
+        std::string target_topic = "/global_map_terrain_type";
 
-        map_manager1_.loadImage(filename, resolution, origin, frame_id);
-        //map_manager1_.printImage();
+        map_terrain_manager_.loadImage(filename, resolution, origin, frame_id);
+        global_terrain_grid_ = map_terrain_manager_.getOccupancyMessage();
 
         //Define publisher
-        publisher1_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(target_topic, rclcpp::QoS(rclcpp::KeepLast(10)).reliable().transient_local());
-        timer1_ = this->create_wall_timer(500ms, std::bind(&MapManagerServer::publisher1_callback_, this));
+        publisher_global_terrain_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(target_topic, rclcpp::QoS(rclcpp::KeepLast(10)).reliable().transient_local());
+        timer1_ = this->create_wall_timer(500ms, std::bind(&MapManagerServer::publisher_global_terrain_callback_, this));
 
 
-        //Load static Image -- Slope
+        //Load Global Intial Static Image -- Slope Value
         filename = "/home/user/ros/marta_ws/src/map_manager/maps/mars_yard_traversability_areas_borders_slope.pgm";
         frame_id = "map";
-        target_topic = "/map_slope";
+        target_topic = "/global_map_slope";
 
-        map_manager2_.loadImage(filename, resolution, origin, frame_id);
-        //map_manager2_.printImage();
+        map_slope_manager_.loadImage(filename, resolution, origin, frame_id);
+        global_slope_grid_ = map_slope_manager_.getOccupancyMessage();
+        //map_slope_manager_.printImage();
 
-        publisher2_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(target_topic, rclcpp::QoS(rclcpp::KeepLast(10)).reliable().transient_local());
-        timer2_ = this->create_wall_timer(500ms, std::bind(&MapManagerServer::publisher2_callback_, this));
+        publisher_global_slope_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>(target_topic, rclcpp::QoS(rclcpp::KeepLast(10)).reliable().transient_local());
+        timer2_ = this->create_wall_timer(500ms, std::bind(&MapManagerServer::publisher_global_slope_callback_, this));
+
+        //Define services to update the (global) map information
+        update_slope_srv_ = this->create_service<map_manager_srvs::srv::TerrainSlopeUpdate>("/update_global_map_slope", std::bind(&MapManagerServer::update_slope_cb, this, std::placeholders::_1, std::placeholders::_2));
+        update_type_srv_ = this->create_service<map_manager_srvs::srv::TerrainTypeUpdate>("/update_global_map_type", std::bind(&MapManagerServer::update_type_cb, this, std::placeholders::_1, std::placeholders::_2));
     }
 
   private:
-    void publisher1_callback_()
+    void publisher_global_terrain_callback_()
     {
-      auto grid_ = map_manager1_.getOccupancyMessage();
-      publisher1_->publish(grid_);
+      publisher_global_terrain_->publish(global_terrain_grid_);
     }
 
-    void publisher2_callback_()
+    void publisher_global_slope_callback_()
     {
-      auto grid_ = map_manager2_.getOccupancyMessage();
-      publisher2_->publish(grid_);
+      publisher_global_slope_->publish(global_slope_grid_);
     }
-    
-    MapManager map_manager1_, map_manager2_;
+  
+    void update_slope_cb(const std::shared_ptr<map_manager_srvs::srv::TerrainSlopeUpdate::Request> request,
+                         std::shared_ptr<map_manager_srvs::srv::TerrainSlopeUpdate::Response>      response)
+    {
+      float radius = request->inflation_radius[0];
+      int limit = (int)(radius/global_slope_grid_.info.resolution);
+      int max_radius = limit*limit;
+      int index_base, index;
+
+      //TODO put it into MapManager Class ?
+      for(int i = 0; i < (int)(request->points.size()); i++){
+        if(request->inflation_radius.size() > 1){
+          radius = request->inflation_radius[i];
+          limit = (int)(radius/global_slope_grid_.info.resolution);
+        }
+        
+        index_base = ((int)((request->points[i].x - global_slope_grid_.info.origin.position.x)/global_slope_grid_.info.resolution)) +
+                     ((int)((request->points[i].y - global_slope_grid_.info.origin.position.y)/global_slope_grid_.info.resolution))*global_slope_grid_.info.width;
+
+        for(int j = -limit; j <= limit; j++){
+          for(int k = -limit; k <= limit; k++){
+            if(k*k+j*j > max_radius)
+              continue;
+            
+            index = index_base + j + k*global_slope_grid_.info.width;
+
+            if(index < 0 || index >= global_slope_grid_.info.width*global_slope_grid_.info.height)
+              continue;
+
+            global_slope_grid_.data[index] = request->slope_values[i];
+          }
+        }
+      }
+
+      publisher_global_slope_->publish(global_slope_grid_);
+    }
+
+    void update_type_cb(const std::shared_ptr<map_manager_srvs::srv::TerrainTypeUpdate::Request> request,
+                        std::shared_ptr<map_manager_srvs::srv::TerrainTypeUpdate::Response>      response)
+    {
+      float radius = request->inflation_radius[0];
+      int limit = (int)(radius/global_terrain_grid_.info.resolution);
+      int max_radius = limit*limit;
+      int index_base, index;
+
+      //TODO put it into MapManager Class ?
+      for(int i = 0; i < (int)(request->points.size()); i++){
+        if(request->inflation_radius.size() > 1){
+          radius = request->inflation_radius[i];
+          limit = (int)(radius/global_terrain_grid_.info.resolution);
+        }
+        
+        index_base = ((int)((request->points[i].x - global_terrain_grid_.info.origin.position.x)/global_terrain_grid_.info.resolution)) +
+                     ((int)((request->points[i].y - global_terrain_grid_.info.origin.position.y)/global_terrain_grid_.info.resolution))*global_terrain_grid_.info.width;
+
+        for(int j = -limit; j <= limit; j++){
+          for(int k = -limit; k <= limit; k++){
+            if(k*k+j*j > max_radius)
+              continue;
+            
+            index = index_base + j + k*global_terrain_grid_.info.width;
+
+            if(index < 0 || index >= global_terrain_grid_.info.width*global_terrain_grid_.info.height)
+              continue;
+
+            global_terrain_grid_.data[index] = request->terrain_types[i];
+          }
+        }
+      }
+
+      publisher_global_terrain_->publish(global_terrain_grid_);
+    }
+
+
+    MapManager map_terrain_manager_, map_slope_manager_;
     rclcpp::TimerBase::SharedPtr timer1_, timer2_;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr publisher1_, publisher2_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr publisher_global_terrain_, publisher_global_slope_;
+    
+    rclcpp::Service<map_manager_srvs::srv::TerrainSlopeUpdate>::SharedPtr update_slope_srv_;
+    rclcpp::Service<map_manager_srvs::srv::TerrainTypeUpdate>::SharedPtr update_type_srv_;
+
+    nav_msgs::msg::OccupancyGrid global_terrain_grid_, global_slope_grid_;
 };
 
 
